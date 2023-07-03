d62037485b471d1c77990b1ccc78b68f6214d5db
amelie <ameliegitti@gmail.com>
2023-07-03 14:44:13 +0100

    Created the Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Open-source code by Amelie Gitti</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
