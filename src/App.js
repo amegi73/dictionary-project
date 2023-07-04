c7e1c25ac171bf311a8743711eb8326dcb906b72
amelie <ameliegitti@gmail.com>
2023-07-04 15:31:58 +0100

    Rendered the word meanings

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">Dictionary</header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer className="App-footer">
-          <small>Open-source code by Amelie Gitti</small>
-        </footer>
+    <body>
+      <div className="App">
+        <div className="container">
+          <header className="App-header">Dictionary</header>
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="App-footer">
+            <small>
+              <a
+                href="https://github.com/amegi73/dictionary-project"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Open-source code
+              </a>{" "}
+              by Amelie Gitti
+            </small>
+          </footer>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
